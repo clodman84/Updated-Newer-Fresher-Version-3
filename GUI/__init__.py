d4cca5166e3f0e1eaa8f7b8e4f0a89c55df37c30
clodman84 <cshourin@gmail.com>
2024-07-29 15:09:19 +0530

    refactored a lil

--- GUI/__init__.py
+++ GUI/__init__.py
@@ -1,4 +1,4 @@
 from .bill import BillingWindow, show_all_nicks
 from .image import ImageWindow
 from .music import MusicVisualiser
-from .utils import Logger, modal_message
+from .utils import Logger, logger_stress_test, modal_message
