ae7356b3ce1ddc7e6d2bb06199924dc1a78ccf30
clodman84 <cshourin@gmail.com>
2026-04-10 23:25:06 +0530

    bleh

--- bundle_tools.cmake
+++ bundle_tools.cmake
@@ -1,16 +1,35 @@
-find_package(Python3 REQUIRED)
-
 function(bundle_runtime_dependencies TARGET_NAME BUNDLE_DIR)
     # 1. Discover shared library dependencies
-    # We use a helper variable to pass the bundle dir to the install script
+    
+    # Define platform-specific exclusions
+    set(_pre_exclude "")
+    set(_post_exclude "")
+
+    if(WIN32)
+        # Windows: Exclude low-level API sets and core OS directories
+        set(_pre_exclude "api-ms-win-.*" "ext-ms-win-.*")
+        set(_post_exclude "(?i)^[A-Z]:/Windows/.*")
+    elseif(APPLE)
+        # macOS: Exclude Apple's core system libraries and frameworks.
+        # (Homebrew/MacPorts libs live in /opt/homebrew, /usr/local, or /opt/local, so they get bundled)
+        set(_post_exclude "^/usr/lib/.*" "^/System/.*")
+    else()
+        # Linux: We CANNOT exclude /usr/lib broadly because system GEGL lives there.
+        # Instead, exclude specific foundational OS and desktop libraries.
+        set(_post_exclude 
+            ".*/lib(gcc_s|stdc\\+\\+|pthread|dl|rt|m|c|resolv)\\.so.*"
+            ".*/lib(X11|Xext|xcb|GL|EGL|GLX|fontconfig|freetype|drm)\\.so.*"
+        )
+    endif()
+
     install(CODE "
-        set(_bundle_dest \"${CMAKE_INSTALL_PREFIX}/${BUNDLE_DIR}/lib\")
+        set(_bundle_dest \"\${CMAKE_INSTALL_PREFIX}/${BUNDLE_DIR}/lib\")
         file(GET_RUNTIME_DEPENDENCIES
             EXECUTABLES \$<TARGET_FILE:${TARGET_NAME}>
             RESOLVED_DEPENDENCIES_VAR _resolved_deps
             UNRESOLVED_DEPENDENCIES_VAR _unresolved_deps
-            PRE_EXCLUDE_REGEXES \"api-ms-win-.*\" \"ext-ms-win-.*\"
-            POST_EXCLUDE_REGEXES \"^/usr/lib/\" \"^/lib/\" \"^/system/\" \"^/System/\" \"^/usr/lib64/\" \"^/lib64/\" \"^/usr/lib/x86_64-linux-gnu/lib(gcc_s|stdc\\+\\+|pthread|dl|rt|m|c)\\.so.*\"
+            PRE_EXCLUDE_REGEXES \${_pre_exclude}
+            POST_EXCLUDE_REGEXES \${_post_exclude}
         )
         foreach(_lib \${\_resolved_deps})
             file(INSTALL \${\_lib} DESTINATION \${\_bundle_dest} FOLLOW_SYMLINK_CHAIN)
