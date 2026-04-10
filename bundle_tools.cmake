ce7e113157100eebc09d8b22e102fd2e8c5b2d93
clodman84 <cshourin@gmail.com>
2026-04-11 00:44:27 +0530

    beeeeeeeeeeeeeeeeeeeees

--- bundle_tools.cmake
+++ bundle_tools.cmake
@@ -1,36 +1,38 @@
 function(bundle_runtime_dependencies TARGET_NAME BUNDLE_DIR)
-    # Define platform-specific exclusions
-    set(_pre_exclude "")
-    set(_post_exclude "")
-
+    # Define platform-specific exclusions (Evaluated at configure time)
     if(WIN32)
-        # Windows: Exclude low-level API sets and core OS directories
-        set(_pre_exclude "api-ms-win-.*" "ext-ms-win-.*")
-        set(_post_exclude "(?i)^([a-z]:)?/windows/.*") 
+        set(_pre "PRE_EXCLUDE_REGEXES" "\"api-ms-win-.*\"" "\"ext-ms-win-.*\"")
+        set(_post "POST_EXCLUDE_REGEXES" "\"(?i)^([a-z]:)?/windows/.*\"") 
     elseif(APPLE)
-        # macOS: Exclude Apple's core system libraries
-        set(_post_exclude "^/usr/lib/.*" "^/System/.*")
+        set(_pre "")
+        set(_post "POST_EXCLUDE_REGEXES" "\"^/usr/lib/.*\"" "\"^/System/.*\"")
     else()
-        # Linux: Universally exclude glibc, C++ standard libs, and core windowing/graphics drivers
-        # Using .* ignores the absolute path (catches /lib, /usr/lib, /lib64, etc.)
-        set(_post_exclude 
-            ".*/lib(gcc_s|stdc\\+\\+|pthread|dl|rt|m|c|resolv)\\.so.*"
-            ".*/ld-linux.*\\.so.*"
-            ".*/lib(X11|Xext|xcb|GL|EGL|GLX|fontconfig|freetype|drm|wayland-.*)\\.so.*"
+        set(_pre "")
+        # Using stdc.* instead of stdc++ to avoid backslash escaping issues
+        set(_post 
+            "POST_EXCLUDE_REGEXES"
+            "\".*/lib(gcc_s|stdc.*|pthread|dl|rt|m|c|resolv)\\\\.so.*\""
+            "\".*/ld-linux.*\\\\.so.*\""
+            "\".*/lib(X11|Xext|xcb|GL|EGL|GLX|fontconfig|freetype|drm|wayland-.*)\\\\.so.*\""
         )
     endif()
+
+    # Convert CMake lists into a space-separated string to safely inject into the install script
+    string(REPLACE ";" " " _pre_str "${_pre}")
+    string(REPLACE ";" " " _post_str "${_post}")
 
     install(CODE "
         set(_bundle_dest \"\${CMAKE_INSTALL_PREFIX}/${BUNDLE_DIR}/lib\")
         file(GET_RUNTIME_DEPENDENCIES
-            EXECUTABLES \$<TARGET_FILE:${TARGET_NAME}>
+            EXECUTABLES $<TARGET_FILE:${TARGET_NAME}>
             RESOLVED_DEPENDENCIES_VAR _resolved_deps
             UNRESOLVED_DEPENDENCIES_VAR _unresolved_deps
-            PRE_EXCLUDE_REGEXES \${_pre_exclude}
-            POST_EXCLUDE_REGEXES \${_post_exclude}
+            ${_pre_str}
+            ${_post_str}
         )
-        foreach(_lib \${\_resolved_deps})
-            file(INSTALL \${\_lib} DESTINATION \${\_bundle_dest} FOLLOW_SYMLINK_CHAIN)
+        foreach(_lib \${_resolved_deps})
+            # Added quotes around \${_lib} to handle potential spaces in paths safely
+            file(INSTALL \"\${_lib}\" DESTINATION \"\${_bundle_dest}\" FOLLOW_SYMLINK_CHAIN)
         endforeach()
     " COMPONENT Runtime)
 
@@ -54,7 +56,7 @@ function(bundle_runtime_dependencies TARGET_NAME BUNDLE_DIR)
         set(_ops \"${REQUIRED_GEGL_OPS}\")
         string(REPLACE \"\\n\" \";\" _ops_list \"\${_ops}\")
         foreach(_op \${_ops_list})
-            file(INSTALL \${_op} DESTINATION \${_gegl_dest} FOLLOW_SYMLINK_CHAIN)
+            file(INSTALL \"\${_op}\" DESTINATION \"\${_gegl_dest}\" FOLLOW_SYMLINK_CHAIN)
         endforeach()
 
         # Copy BABL extensions
